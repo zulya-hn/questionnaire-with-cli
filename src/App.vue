fb47cdd2024f3c99d40998059156412c91ce21bd
zulya <zulya.h95@gmail.com>
2020-10-08 20:41:18 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="wrapper">
+    <div class="sample">
+      
+      <template v-if="!formSubmitted">
+        <app-questions v-for="(question, questIndex) in info"
+                       :question="question"
+                       :questIndex="questIndex"
+                       :answers="answers"
+                       :currentQuestion="currentQuestion"
+                       :isGoNext="isGoNext"
+                       :key="question.id"
+                       @choose-answer="pushAnswer(questIndex, $event)"
+                       @go-next="goNext">
+        
+        </app-questions>
+      </template>
+      
+      <template v-else>
+        <app-answers :answers="answers"
+                     :info="info">
+        </app-answers>
+      </template>
+    
+    </div>
+  </div>
+</template>
+
+<script>
+    import AppQuestions from './components/Questions';
+    import AppAnswers from './components/Answers';
+    
+    export default {
+        data() {
+            return {
+                currentQuestion: 0,
+                info: [
+                    {
+                        id: 101,
+                        text: 'Какие из этих тегов строчные?',
+                        variants: ['a', 'div', 'span', 'img'],
+                        correctVariants: [0, 2, 3],
+                        type: 'checkbox',
+                    },
+                    {
+                        id: 102,
+                        text: 'Какой тег задаёт ссылку?',
+                        variants: ['a', 'div', 'span', 'img'],
+                        correctVariants: [0],
+                        type: 'radio',
+                    },
+                ],
+                answers: [],
+                isGoNext: [],
+            };
+        },
+        methods: {
+            pushAnswer(questIndex, {variant, varNum}) {
+                if (this.answers[questIndex] === undefined) {
+                    this.answers[questIndex] = [];
+                }
+                
+                if (this.info[questIndex].type === 'checkbox') {
+                    
+                    if (this.answers[questIndex][varNum] === undefined) {
+                        this.answers[questIndex][varNum] = variant;
+                        this.checkGoNext();
+                        
+                        return;
+                    }
+                    
+                    this.answers[questIndex][varNum] = undefined;
+                    this.checkGoNext();
+                }
+                
+                if (this.info[questIndex].type === 'radio') {
+                    this.answers[questIndex] = [];
+                    this.answers[questIndex][varNum] = variant;
+                    
+                    this.checkGoNext();
+                }
+                
+            },
+            checkGoNext() {
+                this.isGoNext = this.answers.map((questIndex) => {
+                    return questIndex.filter(varNum => varNum !== undefined).length > 0;
+                });
+            },
+            goNext() {
+                if (this.answers[this.currentQuestion]) {
+                    this.currentQuestion++;
+                }
+            },
+        },
+        computed: {
+            formSubmitted: function () {
+                return this.currentQuestion === this.info.length;
+            }
+        },
+        components: {
+            AppQuestions,
+            AppAnswers
+        }
+    };
+    
+    /*
+                 this.answers = [];
+           =>
+                 this.answers[0] = undefined
+                 this.answers[1] = undefined
+                 ...
+                 this.answers[500] = undefined
+                 
+                 this.answers[0] = []
+           =>
+                 this.answers[0][0] = undefined
+                 this.answers[0][1] = undefined
+                 ...
+                 this.answers[0][500] = undefined
+                 
+                 
+                 this.answers[1][0]
+                 this.answers[1][1]
+                 this.answers[1][2]
+                 
+                 
+                 this.answers[0][0] = div
+                 this.answers[0][1] = span
+                 =>
+                 this.answers[0] = [div, span]
+                 
+                 this.answers[0] = []
+                 =>
+                 this.answers[0][0] = undefined
+                 this.answers[0][1] = undefined
+           */
+
+</script>
+
+<style scoped>
+  .wrapper {
+    max-width: 600px;
+    margin: 20px auto;
+  }
+
+</style>
---
+++ src/components/Answers.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+      
+        <h3>Right answers:</h3>
+        
+        <div v-for="(question, questIndex) in info">
+          <br>
+          <span class="mt-5 mb-3">
+                    {{ (questIndex + 1) + ') ' + question.text }}
+                </span>
+          <br>
+          <span v-for="corVar in question.correctVariants"
+                class="form-check correct">
+                    {{ question.variants[corVar] }}
+                </span>
+        </div>
+        
+        <hr>
+        
+        <h3>Your answers:</h3>
+      
+        <div v-for="(question, questIndex) in info">
+          <br>
+          <span class="mt-5 mb-3">
+                {{ (questIndex + 1) + ') ' + question.text }}
+            </span>
+          <br>
+          <span class="mt-5 mb-3"
+                  :class="correctOrWrong(index, question.correctVariants)"
+                  v-for="(variant, index) in answers[questIndex]">
+                {{ variant }}
+            </span>
+        </div>
+  
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            'answers': Array,
+            'info': Array,
+        },
+        computed: {
+        
+        },
+        methods: {
+            correctOrWrong(userAnswer, correctAnswers) {
+                return correctAnswers.includes(userAnswer) ? 'correct' : 'wrong';
+            }
+        }
+    };
+</script>
+
+<style scoped>
+  
+  .correct {
+    color: #4CAE4C;
+  }
+  
+  .wrong {
+    color: red;
+  }
+  
+
+</style>
