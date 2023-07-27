006f3b4fc5d94d1587cb6d43a3f3d9969c9fe192
Zulfiya <zulya.h95@mail.ru>
2023-07-27 12:44:37 +0300

    fixes

--- src/components/Answers.vue
+++ src/components/Answers.vue
@@ -1,68 +1,65 @@
 <template>
-    <div>
-      
-        <h3>Right answers:</h3>
-        
-        <div v-for="(question, questIndex) in info">
-          <br>
-          <span class="mt-5 mb-3">
-                    {{ (questIndex + 1) + ') ' + question.text }}
-                </span>
-          <br>
-          <span v-for="corVar in question.correctVariants"
-                class="form-check correct">
-                    {{ question.variants[corVar] }}
-                </span>
-        </div>
-        
-        <hr>
-        
-        <h3>Your answers:</h3>
-      
-        <div v-for="(question, questIndex) in info">
-          <br>
-          <span class="mt-5 mb-3">
-                {{ (questIndex + 1) + ') ' + question.text }}
-            </span>
-          <br>
-          <span class="mt-5 mb-3"
-                  v-for="(variant, variantNumber) in answers[questIndex]"
-                
-                  :class="correctOrWrong(variantNumber, question.correctVariants)">
-            
-                {{ variant }}
-            </span>
-        </div>
-  
+  <div>
+    <h3>Right answers:</h3>
+
+    <div v-for="(question, questIndex) in info" :key="`quest-${questIndex}`">
+      <br />
+      <span class="mt-5 mb-3">
+        {{ questIndex + 1 + ") " + question.text }}
+      </span>
+      <br />
+      <span
+        v-for="(corVar, corVarIndex) in question.correctVariants"
+        class="form-check correct"
+        :key="`corvar-${corVarIndex}`"
+      >
+        {{ question.variants[corVar] }}
+      </span>
     </div>
+
+    <hr />
+
+    <h3>Your answers:</h3>
+
+    <div v-for="(question, questIndex) in info" :key="`question-${questIndex}`">
+      <br />
+      <span class="mt-5 mb-3">
+        {{ questIndex + 1 + ") " + question.text }}
+      </span>
+      <br />
+      <span
+        class="mt-5 mb-3"
+        v-for="(variant, variantNumber) in answers[questIndex]"
+        :key="`variant-${variantNumber}`"
+        :class="correctOrWrong(variantNumber, question.correctVariants)"
+      >
+        {{ variant }}
+      </span>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            'answers': Array,
-            'info': Array,
-        },
-        computed: {
-        
-        },
-        methods: {
-            correctOrWrong(userVariant, correctVariants) {
-                return correctVariants.includes(userVariant) ? 'correct' : 'wrong';
-            }
-        }
-    };
+export default {
+  props: {
+    answers: Array,
+    info: Array,
+  },
+  computed: {},
+  methods: {
+    correctOrWrong(userVariant, correctVariants) {
+      return correctVariants.includes(userVariant) ? "correct" : "wrong";
+    },
+  },
+};
 </script>
 
 <style scoped>
-  
-  .correct {
-    color: #4CAE4C;
-  }
-  
-  .wrong {
-    color: red;
-  }
-  
+.correct {
+  color: #4cae4c;
+}
 
+.wrong {
+  color: red;
+}
 </style>
--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,51 +1,53 @@
 <template>
-  
   <div v-if="questIndex === currentQuestion">
     <h4 class="mt-5 mb-3">
       {{ question.text }}
     </h4>
-    
-    <div v-for="(variant, varNum) in question.variants"
-         class="form-check">
+
+    <div
+      v-for="(variant, varNum) in question.variants"
+      :key="`index-${varNum}`"
+      class="form-check"
+    >
       <label class="form-check-label">
-        <input class="form-check-input"
-               :type="question.type"
-               @click="chooseAnswer(variant, varNum)"
-        >
-        
+        <input
+          class="form-check-input"
+          :type="question.type"
+          @click="chooseAnswer(variant, varNum)"
+        />
+
         {{ variant }}
-      
       </label>
     </div>
-    
+
     <div class="mt-5">
-      <button class="btn btn-primary"
-              @click="toGoNext"
-              :disabled="!isGoNext[questIndex]"
+      <button
+        class="btn btn-primary"
+        @click="toGoNext"
+        :disabled="!isGoNext[questIndex]"
       >
         Дальше
       </button>
     </div>
   </div>
-  
 </template>
 
 <script>
-    export default {
-        props: {
-            'question': Object,
-            'questIndex': Number,
-            'currentQuestion': Number,
-            'answers': Array,
-            'isGoNext': Array,
-        },
-        methods: {
-            chooseAnswer(variant, varNum) {
-                this.$emit('choose-answer', {variant, varNum});
-            },
-            toGoNext() {
-                this.$emit('go-next');
-            }
-        }
-    };
+export default {
+  props: {
+    question: Object,
+    questIndex: Number,
+    currentQuestion: Number,
+    answers: Array,
+    isGoNext: Array,
+  },
+  methods: {
+    chooseAnswer(variant, varNum) {
+      this.$emit("choose-answer", { variant, varNum });
+    },
+    toGoNext() {
+      this.$emit("go-next");
+    },
+  },
+};
 </script>
